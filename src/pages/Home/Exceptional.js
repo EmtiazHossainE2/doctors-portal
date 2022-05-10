12babb047ad8b54289589e5a877d38a59000bc25
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-10 14:09:51 +0600

    exceptional section added

---
+++ src/pages/Home/Exceptional.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Exceptional = () => {
+    return (
+        <div class="flex items-center justify-center text-center md:space-x-12 md:mx-36 flex-col md:flex-row my-20">
+            <div><img className=' lg:max-w-sm rounded-lg' src="https://i.ibb.co/0BLdtN7/treatment.png" alt="Treatment" /></div>
+            <div >
+                <h2 class="card-title text-4xl font-bold font-sans">Exceptional Dental <br /> Care, on Your Terms</h2>
+                <p className='text-justify my-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <div className=' flex  flex-start'>
+                    <button className="btn btn-primary font-semiBold text-white uppercase bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Exceptional;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Exceptional from './Exceptional';
 import Info from './Info';
 import Services from './Services';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Info/>
             <Services/>
+            <Exceptional/>
         </div>
     );
 };
