6f904075f35cba9ace4c0d319f0e4d039037521f
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-11 23:49:15 +0600

    firebase setup , env file setup , google login implement

--- src/pages/Account/Login/Login.js
+++ src/pages/Account/Login/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../Firebase/firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, googleUser, loading, googleError] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+
+    //user 
+    if(googleUser){
+        navigate('/')
+    }
+
+    //error 
+    let errorMessage;
+    if (googleError) {
+        errorMessage = <p className='text-red-500'> Try Again Please </p>
+    }
+
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='flex h-[40vh] lg:h-[70vh] justify-center items-center'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl">Log In</h2>
+                    
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} class="btn btn-outline uppercase text-lg ">Continue With Google</button>
+                    {errorMessage}
+                </div>
+            </div>
         </div>
     );
 };
