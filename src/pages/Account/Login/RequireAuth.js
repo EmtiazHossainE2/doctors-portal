9cc42c5c8c082518d5991ce4193a12a637828b2b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-12 17:00:06 +0600

    email verification done

--- src/pages/Account/Login/RequireAuth.js
+++ src/pages/Account/Login/RequireAuth.js
@@ -1,19 +1,43 @@
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState, useSendEmailVerification } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
 import { Navigate, useLocation } from "react-router-dom";
 import Loading from "../../../conponents/Loading";
 import auth from "../../../Firebase/firebase.init";
 
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, sendingError] = useSendEmailVerification(auth);
     const location = useLocation();
 
-    if(loading){
-        return <Loading/>
+    if (loading || sending) {
+        return <Loading />
     }
 
-    if(!user){
+    if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace></Navigate>
     }
+
+    else if (user?.emailVerified === false) {
+        return <div className='flex h-[60vh]  justify-center items-center '>
+            <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+                <div className="card-body border-2 lg:border-none">
+                    <h2 className="text-center text-3xl">Your email is not verified .</h2>
+                    <h2 className="text-center text-xl">Please check your email  .</h2>
+                    <button className='btn btn-black text-white   mt-4 py-2 '
+                        onClick={async () => {
+                            await sendEmailVerification();
+                            toast.success('Sent email verification success . Check your email .');
+                        }}
+                    >
+                        Resend Verification
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    }
+
+
     return children;
 };
 
--- src/pages/Account/Signup/Signup.js
+++ src/pages/Account/Signup/Signup.js
@@ -14,7 +14,7 @@ const SignUp = () => {
         user,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth ,{ sendEmailVerification: true });
 
     const navigate = useNavigate();
     const location = useLocation();
