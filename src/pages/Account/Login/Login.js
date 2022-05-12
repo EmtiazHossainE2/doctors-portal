74f40afa30e4e90503ef2441eccc280ec2c39b57
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-12 13:06:37 +0600

    sign in with email pass

--- src/pages/Account/Login/Login.js
+++ src/pages/Account/Login/Login.js
@@ -1,37 +1,51 @@
-import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import React, { useEffect } from 'react';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../../Firebase/firebase.init';
 import toast from 'react-hot-toast';
 import { useForm } from 'react-hook-form';
+import Loading from '../../../conponents/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, googleUser, loading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
     const navigate = useNavigate()
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [signInWithEmailAndPassword,user,loading,error, ] = useSignInWithEmailAndPassword(auth);
 
-    //user 
-    if (googleUser) {
-        navigate('/')
+    useEffect( () =>{
+        if (user || googleUser) {
+            // navigate(from, { replace: true });
+        }
+    }, [googleUser , user])
+
+    if (loading || googleLoading) {
+        return <Loading></Loading>
     }
 
     //error 
-    let errorMessage;
-    if (googleError) {
-        errorMessage = <p className='text-red-500 pt-3 text-lg'> Pop up is Closed by user </p>
-        toast.error(`Something is Wrong,Try Again`, { id: "googleError" });
+    let signInError ;
+    if(error){
+        signInError= <p className='text-red-500 text-lg'>Could not find user</p>
+    }
+    // googleError 
+    let gError ;
+    if(googleError){
+        gError= <p className='text-red-500 text-lg'>Pop up is Closed by user</p>
     }
 
     //login 
     const onSubmit = data => {
         console.log(data)
+        signInWithEmailAndPassword(data.email , data.value)
     };
 
     return (
         <div className='flex h-[60vh] lg:h-[100vh] justify-center items-center '>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body border-2 ">
+                <div className="card-body border-2 lg:border-none">
                     <h2 className="text-center text-2xl">Log In</h2>
+                    {signInError}
+                    {gError}
                     <div>
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div class="form-control w-full max-w-xs">
@@ -81,7 +95,7 @@ const Login = () => {
                                         }
                                     })}
                                 />
-                                <label class="label pb-3">
+                                <label class="label pb-4">
                                 {errors.password?.type === 'required' && <span className="label-text-alt text-red-500 text-[15px]">{errors.password.message}</span>}
                                 {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500 text-[15px]">{errors.password.message}</span>}
                                 {errors.password?.type === 'pattern' && <span className="label-text-alt text-red-500 text-[15px]">{errors.password.message}</span>}
@@ -94,7 +108,6 @@ const Login = () => {
                     </div>
                     <div className="divider">OR</div>
                     <button onClick={() => signInWithGoogle()} className="btn btn-outline uppercase text-lg ">Continue With Google</button>
-                    {errorMessage}
                 </div>
             </div>
         </div>
