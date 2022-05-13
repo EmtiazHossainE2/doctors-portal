2377f349570be0db772af5f4ff13900fa0267f59
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-14 01:53:11 +0600

    booking implement using axios

--- src/pages/Appointment/BookingModal.js
+++ src/pages/Appointment/BookingModal.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import { format } from 'date-fns';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import toast from 'react-hot-toast';
 import auth from '../../Firebase/firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
@@ -9,7 +11,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
 
     const handleBooking = event => {
         event.preventDefault()
-        const bookingValue = {
+        const bookingInfo = {
             treatment: treatment.name,
             date: event.target.date.value,
             slot: event.target.slot.value,
@@ -17,8 +19,30 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             phone: event.target.phone.value,
             email: event.target.email.value
         }
-        console.log(bookingValue);
-        setTreatment(null)
+
+
+        if(bookingInfo.phone === ''){
+            toast.error(`Please Provide Your Contact Number `, { id: "phoneError" });
+            return 
+        }
+        else{
+            axios.post('http://localhost:5000/booking', bookingInfo)
+            .then(function (response) {
+                console.log(response);
+                toast.success(`Your Appointment is successfully booking `, { id: "booking" });
+                setTreatment(null)
+            })
+            .catch(function (error) {
+                console.log(error);
+                toast.error(`Something is wrong . Try later `, { id: "bookingError" });
+            });
+        }
+
+
+        
+
+
+
 
 
     }
@@ -35,7 +59,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         <form onSubmit={handleBooking} className='space-y-4 pt-8 '>
                             <input type="text" name='date' disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-md text-lg " />
                             <select name='slot' className="select select-bordered w-full max-w-md space-y-8 text-lg">
-                                {slots.map((slot , index) =>
+                                {slots.map((slot, index) =>
                                     <option value={slot} key={index}>{slot}</option>)
                                 }
                             </select>
